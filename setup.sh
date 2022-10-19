06b0ddf98e21262837d20e82a9895d6b2c26e2d4
bf_opengradient <40048681+bfopengradient@users.noreply.github.com>
2022-10-19 16:01:31 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 mkdir -p ~/.streamlit/
 
-echo "\
+echo "[theme]
+primaryColor = "#7792E3"
+backgroundColor = "#073346"
+secondaryBackgroundColor = "#2d3c52"
+textColor = "#FFFFFF"
+font = "sans serif"
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
